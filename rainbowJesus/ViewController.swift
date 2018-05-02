3a9a75cf50c3359ee32d21d20d90da6dcebd2156
Chang Sophia <gmb728@gmail.com>
2018-05-02 08:36:35 +0800

    Initial Commit

---
+++ rainbowJesus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainbowJesus
+//
+//  Created by Chang Sophia on 2018/5/2.
+//  Copyright © 2018年 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
